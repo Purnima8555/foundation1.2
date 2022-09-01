65c8b8ca3d0c31abfcce267d1ad70daa515449e1
Purnima8555 <110591365+Purnima8555@users.noreply.github.com>
2022-09-01 17:50:54 +0545

    Add files via upload

---
+++ foundation1.2/homework.py
@@ -0,0 +1,11 @@
+#1 Describing my pet:
+
+Pet_Name = input("Furchu")
+Animal_type = input("Dog")
+
+def describe_pet(animal_type, pet_name):
+
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name}")
+
+
